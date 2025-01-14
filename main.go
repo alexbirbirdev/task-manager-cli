61590fbaeb1b53c5693db37a8d5df85a9d1ff1aa
Alex <alexbirdev@gmail.com>
2025-01-14 18:12:29 +0300

    added mark-done functional

--- main.go
+++ main.go
@@ -80,6 +80,8 @@ func main() {
 			fmt.Println("Usage: go run mark-in-progress <id>")
 			os.Exit(1)
 		}
+		id, _ := strconv.Atoi(os.Args[2])
+		markDone(tasks, id)
 	case "list":
 		var status string
 		if len(os.Args) == 3 {
@@ -210,7 +212,7 @@ func markInProgress(tasks []Task, id int) ([]Task, error) {
 	for i, task := range tasks {
 		if task.ID == id {
 			found = true
-			task.Status = "in progress"
+			task.Status = "In progress"
 			tasks[i] = task
 		}
 	}
@@ -220,7 +222,39 @@ func markInProgress(tasks []Task, id int) ([]Task, error) {
 	}
 
 	// Сохраняем изменения в JSON-файл
-	file, err := os.OpenFile("tasks.json", os.O_RDWR|os.O_CREATE, 0644)
+	file, err := os.OpenFile("tasks.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Println("Error opening file", err)
+		return tasks, err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", " ")
+	err = encoder.Encode(tasks)
+	if err != nil {
+		return tasks, fmt.Errorf("Error: %s", err)
+	}
+
+	return tasks, nil
+}
+
+func markDone(tasks []Task, id int) ([]Task, error) {
+	var found bool
+	for i, task := range tasks {
+		if task.ID == id {
+			found = true
+			task.Status = "Done"
+			tasks[i] = task
+		}
+	}
+
+	if !found {
+		return tasks, fmt.Errorf("Task with ID %v not found", id)
+	}
+
+	// Сохраняем изменения в JSON-файл
+	file, err := os.OpenFile("tasks.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
 		fmt.Println("Error opening file", err)
 		return tasks, err
